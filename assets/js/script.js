fa5a62bbd1d7fece394c0e00e28d1f3f564a9b61
michel2407 <62596237+michel2407@users.noreply.github.com>
2020-05-18 08:37:03 +0200

    Cv numérique avec mise en place d'un format vidéo fonctionnel

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+function viewlevel(){
+  document.getElementById('level').src='<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star-half-alt"></i><i class="far fa-star"></i><i class="far fa-star"alt=""></i>';
+}
